35fdc1a2fe5ef6722752177b99ee687daf33ca19
furandolu <166970841+furandolu@users.noreply.github.com>
2024-05-31 17:06:55 +0900

    Create AboutActivity.kt

---
+++ app/java/com.reimu.myapp1/AboutActivity.kt
@@ -0,0 +1,9 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class AboutActivity : AppCompatActivity() {
+textView.text = "Reimu Browser"
+textView.text = "バージョン 1.0"
+textView.text = "Created by Hakurei Reimu"
+}

92367c05b0bc38d24447ec29431e9baad1c6af7c
furandolu <166970841+furandolu@users.noreply.github.com>
2024-05-31 17:15:29 +0900

    Update AboutActivity.kt

--- app/java/com.reimu.myapp1/AboutActivity.kt
+++ app/java/com.reimu.myapp1/AboutActivity.kt
@@ -7,4 +7,15 @@ imageView.setImageResource(R.mipmap.ic_about)
 textView.text = "Reimu Browser"
 textView.text = "バージョン 1.0"
 textView.text = "Created by Hakurei Reimu"
+val clickableSpan = object : URLSpan("https://github.com/furandolu/Android-App-1") {
+	override fun updateDrawState(ds: TextPaint) {
+		super.updateDrawState(ds)
+		ds.isUnderlineText = false
+	}
+}
+binding.text.text = SpannableString("Github").apply {
+	setSpan(clickableSpan, 3, 6, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+}
+binding.text.movementMethod = LinkMovementMethod.getInstance()
+
 }
